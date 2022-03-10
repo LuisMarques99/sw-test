c623e702919dd0338d03cad5e7a350df7386313d
Luis Marques <luisserafim99@gmail.com>
2022-03-10 21:54:40 +0000

    Base directory fixed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js', { scope: './sw-test/' })
+    navigator.serviceWorker.register('./sw.js')
         .then((reg) => {
             // Registration worked
             console.log(`Registration succeeded. Scope is ${reg.scope}`);
